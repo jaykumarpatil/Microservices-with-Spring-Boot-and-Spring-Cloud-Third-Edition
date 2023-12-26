d6d3e62ee38c8a271967d3f0c0d906eeced3e14c
itsme <jaypatil3004@gmail.com>
2023-12-26 12:32:38 +0530

    Added Persistence

---
+++ microservices/review-service/src/test/java/com/example/microservices/core/review/TestApplication.java
@@ -0,0 +1,11 @@
+package com.example.microservices.core.review;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+  public static void main(String[] args) {
+    SpringApplication.from(ReviewServiceApplication::main)
+      .with(TestApplicationConfiguration.class)
+      .run(args);
+  }
+}
